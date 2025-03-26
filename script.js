68f0e27acc343faa4d73f84b4861afe815c26565
BbeckerRE <Bradley.beck3r@gmail.com>
2025-03-25 22:17:57 -0500

    "adding comments and functionality to make multiple randomized adventures"

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//establishing arrays and storing all array data within an object
 let adventureData = {
     times: ["morning", "afternoon", "evening", "night"],
     conditions: ["stormy", "foggy", "icy", "sweltering", "crystalline", "misty", "blistering", "chilly", "blazing", "gloomy", 
@@ -7,13 +8,25 @@ let adventureData = {
 };
 
 
-
+//creates one randomized adventure.
 const randomizedAdventure = () => {
     let randomTime = Math.floor(Math.random() * adventureData.times.length);
     let randomCondition = Math.floor(Math.random() * adventureData.conditions.length);
     let randomLocation = Math.floor(Math.random() * adventureData.locations.length);
 
-    console.log(`Your story begins on a ${adventureData.conditions[randomCondition]} ${adventureData.times[randomTime]} in the far away ${adventureData.locations[randomLocation]} of these cursed lands.`);
+    console.log(`Your story begins on a ${adventureData.conditions[randomCondition]} ${adventureData.times[randomTime]} in a far away ${adventureData.locations[randomLocation]} of these cursed lands.`);
 }
 
-randomizedAdventure();
+//creates a variable number of randomized adventures.
+const manyRandomizedAdventures = (num) => {
+    for (i = 0; i < num; i++) {
+    let randomTime = Math.floor(Math.random() * adventureData.times.length);
+    let randomCondition = Math.floor(Math.random() * adventureData.conditions.length);
+    let randomLocation = Math.floor(Math.random() * adventureData.locations.length);
+
+    console.log(`Your story begins on a ${adventureData.conditions[randomCondition]} ${adventureData.times[randomTime]} in a far away ${adventureData.locations[randomLocation]} of these cursed lands.`);
+    }
+}
+
+//randomizedAdventure();
+manyRandomizedAdventures(10);
